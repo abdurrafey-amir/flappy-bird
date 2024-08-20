332bc23763a180f8fe1203a513c6b5a507e4d2fe
abdurrafey-amir <abdurrafeyamir15@gmail.com>
2024-08-20 23:57:22 +0500

    player added, worked on reset and collisions

--- main.py
+++ main.py
@@ -31,19 +31,29 @@ gravity = 1.5
 
 
 # rects
-player = pygame.FRect(50, HEIGHT / 2, 30, 30)
-pipe_bottom1 = pygame.FRect(200, HEIGHT - 250, 50, 320)
-pipe_top1 = pygame.FRect(200, 0, 50, 320)
-pipe_bottom2 = pygame.FRect(400, HEIGHT - 250, 50, 320)
-pipe_top2 = pygame.FRect(400, 0, 50, 320)
+# player = pygame.FRect(50, HEIGHT / 2, 30, 30)
+player_surf = pygame.image.load('assets/player.png').convert_alpha()
+player_surf = pygame.transform.scale(player_surf, (65, 65))
+player = player_surf.get_frect(center = (50, HEIGHT / 2))
+
+# pipes
+pipe_bottom1 = pygame.FRect(200, HEIGHT - 150, 50, 310)
+pipe_top1 = pygame.FRect(200, 0, 50, 310)
+pipe_bottom2 = pygame.FRect(400, HEIGHT - 250, 50, 310)
+pipe_top2 = pygame.FRect(400, -100, 50, 310)
 
 # sounds
 flap_sound = pygame.mixer.Sound('assets/flap.mp3')
 die_sound = pygame.mixer.Sound('assets/die.mp3')
 point_sound = pygame.mixer.Sound('assets/point.mp3')
 
+# font
+font = pygame.font.Font("freesansbold.ttf", 24)
+score = 0
+
 # functions
 def player_ani(player):
+    global score
     player.y += player_jump
     player.y += gravity
     player.x += player_speed
@@ -55,14 +65,17 @@ def player_ani(player):
         player.top = 0 
 
     # collisions
-    if player.colliderect(pipe_bottom1) or player.colliderect(pipe_bottom2) or player.colliderect(pipe_top1) or player.colliderect(pipe_top2):
-        pygame.mixer.Sound.play(die_sound)
-        player.x = 50
+    # if (abs(player.right - pipe_bottom1.left) <= 10) or (abs(player.right - pipe_bottom2.left) <= 10) or (abs(player.right - pipe_top1.left) <= 10) or (abs(player.right - pipe_top2.left) <= 10):
+    #     if (abs(player.bottom - pipe_bottom1.top) <= 10) or (abs(player.bottom - pipe_bottom2.top) <= 10) or (abs(player.top - pipe_top1.bottom) <= 10) or (abs(player.top - pipe_top2.bottom) <= 10):
+    if (player.colliderect(pipe_bottom1) or player.colliderect(pipe_bottom2) or player.colliderect(pipe_top1) or player.colliderect(pipe_top2)):
+                pygame.mixer.Sound.play(die_sound)
+                reset()
 
     # pipes
     if player.right >= WIDTH:
         pygame.mixer.Sound.play(point_sound)
         player.x = 50
+        score += 1
         
         pipe_1_x = random.randint(100, 250)
         pipe_2_x = random.randint(250, 450)
@@ -70,6 +83,9 @@ def player_ani(player):
         if (pipe_1_x - pipe_2_x < 100):
             pipe_1_x = random.randint(100, 250)
             pipe_2_x = random.randint(250, 400)
+            if (pipe_1_x - pipe_2_x < 100):
+                pipe_1_x = random.randint(100, 250)
+                pipe_2_x = random.randint(250, 400)
 
         # while (pipe_1 - pipe_2 < 100) or (pipe_1 < 100) or (pipe_2 < 100):
         #     pipe_1 = random.randint(100, 250)
@@ -81,10 +97,10 @@ def player_ani(player):
         pipe_bottom2.x = pipe_2_x
         pipe_top2.x = pipe_2_x
 
-        pipe_bottom1_y = random.randint(335, HEIGHT)
-        pipe_top1_y = random.randint(0, 335)
-        pipe_bottom2_y = random.randint(335, HEIGHT)
-        pipe_top2_y = random.randint(0, 335)
+        pipe_bottom1_y = random.randint(335, 450)
+        pipe_top1_y = random.randint(200, 335)
+        pipe_bottom2_y = random.randint(335, 450)
+        pipe_top2_y = random.randint(200, 335)
 
     
         pipe_bottom1.top = pipe_bottom1_y
@@ -92,9 +108,27 @@ def player_ani(player):
         pipe_bottom2.top = pipe_bottom2_y
         pipe_top2.bottom = pipe_top2_y
 
+    
         
 
-    
+def reset():
+    global score
+    player.x = 50
+    player.y = HEIGHT / 2
+    pipe_bottom1.left = 200
+    pipe_top1.left = 200
+    pipe_bottom2.left = 400
+    pipe_top2.left = 400
+    pipe_bottom1.top = HEIGHT - 150
+    pipe_top1.top = 0
+    pipe_bottom2.top = HEIGHT - 250
+    pipe_top2.top = -100
+    score = 0
+
+# pipe_bottom1 = pygame.FRect(200, HEIGHT - 150, 50, 310)
+# pipe_top1 = pygame.FRect(200, 0, 50, 310)
+# pipe_bottom2 = pygame.FRect(400, HEIGHT - 250, 50, 310)
+# pipe_top2 = pygame.FRect(400, -100, 50, 310)
 
 
 # main loop
@@ -129,8 +163,12 @@ while running:
     if bg_y <= -1000:
         bg_y = -500
     screen.blit(background, (bg_y, 0))
+    # score
+    score_text = font.render(f'Score: {score}', False, grey)
+    screen.blit(score_text, (WIDTH / 2 - 50, HEIGHT / 2))
+    
     # player
-    player = pygame.draw.rect(screen, white, player, 0, 12)
+    player = screen.blit(player_surf, player)
     player_ani(player)
     # player.y += gravity
     pygame.draw.rect(screen, white, pipe_bottom1)
