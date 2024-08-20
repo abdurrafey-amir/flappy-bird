f8373f1d21b159ea6a86aa81b732abb2f80bfc02
abdurrafey-amir <abdurrafeyamir15@gmail.com>
2024-08-20 18:12:37 +0500

    player movement and pipes

--- main.py
+++ main.py
@@ -16,19 +16,20 @@ grey = pygame.Color('grey12')
 white = (255, 255, 255)
 
 # pos variables
-player_x = 50
-player_y = HEIGHT / 2
 player_jump = 0
-gravity = -1 
+gravity = 1
+
+
+# rects
+player = pygame.Rect(50, HEIGHT / 2, 30, 30)
+pipe_bottom = pygame.Rect(100, HEIGHT - 50, 50, 50)
+pipes = []
 
 # functions
-def player_render(x, y):
-    player_rect = pygame.Rect(x, y, 30, 30)
-    player = pygame.draw.rect(screen, white, player_rect, 0, 12)
-    return player
-
 def player_ani(player):
     player.y += player_jump
+    player.y += gravity
+    # print(player.y)
     if player.bottom >= HEIGHT:
         player.bottom = HEIGHT
     if player.top <= 0:
@@ -50,17 +51,24 @@ while running:
         # inputs
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                player_jump += 10
+                player_jump -= 10
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_SPACE:
-                player.y += gravity
+                # player.y += gravity
+                player_jump += 10
+                
 
     # display
     screen.fill(grey)
     # player
-    player = player_render(player_x, player_y)
+    player = pygame.draw.rect(screen, white, player, 0, 12)
     player_ani(player)
-    player.y += gravity
+    # player.y += gravity
+
+    for i in range(2):
+        pipes.append(pipe_bottom)
+    for pipe in pipes:
+        pygame.draw.rect(screen, black, pipe)
 
 
     # update display
