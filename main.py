0a4eb410d2c75354c312de9633ade00649cd9e99
abdurrafey-amir <abdurrafeyamir15@gmail.com>
2024-08-20 22:33:35 +0500

    worked on pipes, sounds

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import random
 
 
 # general setup
+pygame.mixer.pre_init(44100, -16, 2, 512)
 pygame.init()
 # pygame.camera.init()
 
@@ -19,7 +20,7 @@ clock = pygame.time.Clock()
 black = (0, 0, 0)
 grey = pygame.Color('grey12')
 white = (255, 255, 255)
-background = pygame.image.load('background.jpg')
+background = pygame.image.load('assets/background.jpg')
 bg_x = 0
 bg_y =  0
 
@@ -31,10 +32,15 @@ gravity = 1.5
 
 # rects
 player = pygame.FRect(50, HEIGHT / 2, 30, 30)
-pipe_bottom1 = pygame.FRect(200, HEIGHT - 250, 50, 250)
-pipe_top1 = pygame.FRect(200, 0, 50, 250)
-pipe_bottom2 = pygame.FRect(400, HEIGHT - 250, 50, 250)
-pipe_top2 = pygame.FRect(400, 0, 50, 250)
+pipe_bottom1 = pygame.FRect(200, HEIGHT - 250, 50, 320)
+pipe_top1 = pygame.FRect(200, 0, 50, 320)
+pipe_bottom2 = pygame.FRect(400, HEIGHT - 250, 50, 320)
+pipe_top2 = pygame.FRect(400, 0, 50, 320)
+
+# sounds
+flap_sound = pygame.mixer.Sound('assets/flap.mp3')
+die_sound = pygame.mixer.Sound('assets/die.mp3')
+point_sound = pygame.mixer.Sound('assets/point.mp3')
 
 # functions
 def player_ani(player):
@@ -50,17 +56,43 @@ def player_ani(player):
 
     # collisions
     if player.colliderect(pipe_bottom1) or player.colliderect(pipe_bottom2) or player.colliderect(pipe_top1) or player.colliderect(pipe_top2):
+        pygame.mixer.Sound.play(die_sound)
         player.x = 50
 
     # pipes
     if player.right >= WIDTH:
+        pygame.mixer.Sound.play(point_sound)
         player.x = 50
-        pipe_1 = random.randint(0, 250)
-        pipe_bottom1.x = pipe_1
-        pipe_top1.x = pipe_1
-        pipe_2 = random.randint(250, 400)
-        pipe_bottom2.x = pipe_2
-        pipe_top2.x = pipe_2
+        
+        pipe_1_x = random.randint(100, 250)
+        pipe_2_x = random.randint(250, 450)
+
+        if (pipe_1_x - pipe_2_x < 100):
+            pipe_1_x = random.randint(100, 250)
+            pipe_2_x = random.randint(250, 400)
+
+        # while (pipe_1 - pipe_2 < 100) or (pipe_1 < 100) or (pipe_2 < 100):
+        #     pipe_1 = random.randint(100, 250)
+        #     pipe_2 = random.randint(250, 400)
+
+        pipe_bottom1.x = pipe_1_x
+        pipe_top1.x = pipe_1_x
+        
+        pipe_bottom2.x = pipe_2_x
+        pipe_top2.x = pipe_2_x
+
+        pipe_bottom1_y = random.randint(335, HEIGHT)
+        pipe_top1_y = random.randint(0, 335)
+        pipe_bottom2_y = random.randint(335, HEIGHT)
+        pipe_top2_y = random.randint(0, 335)
+
+    
+        pipe_bottom1.top = pipe_bottom1_y
+        pipe_top1.bottom = pipe_top1_y
+        pipe_bottom2.top = pipe_bottom2_y
+        pipe_top2.bottom = pipe_top2_y
+
+        
 
     
 
@@ -79,6 +111,7 @@ while running:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 player_jump -= 10
+                pygame.mixer.Sound.play(flap_sound)
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_SPACE:
                 # player.y += gravity
